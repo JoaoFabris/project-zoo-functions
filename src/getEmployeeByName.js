ee278e890ecfee983150fa6a20e2c449a7651f78
Joao <jvfabris21@gmail.com>
2022-05-21 17:30:45 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
-
+const { employees } = require('../data/zoo_data'); // desestrutura.
+// Busque as pessoas colaboradoras pelo primeiro ou último nome delas.
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName)
+    || {};
 }
 
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,5 +4,5 @@ const { species } = require('../data/zoo_data'); // desestrutura.
 function getSpeciesByIds(...ids) {
   return ids.map((id) => species.find((specie) => specie.id === id));
 }
-
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
