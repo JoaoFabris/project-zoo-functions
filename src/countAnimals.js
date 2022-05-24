b3b34534bd49353fd3a42197933a2998322a7074
Joao <jvfabris21@gmail.com>
2022-05-24 16:53:22 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
-
+const { species } = require('../data/zoo_data'); // desestrutura.
+// Contabilize a quantidade de espécies de animais residentes no zoológico
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const allAnimals = species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length // acc[specie.name] irá roda dentro do specie.name de um em um para passar o nome do animal, começando com.
+      return acc;
+    }, {});
+    return allAnimals;
+  } if (!animal.sex) {
+    const findAnimal = species.find((animals) => animals.name === animal.specie);
+    return findAnimal.residents.length;
+  }
+  const findAnimalSex = species.find((animals) => animals.name === animal.specie).
+    residents.filter((animalSex) => animalSex.sex === animal.sex).length;
+    return findAnimalSex;
 }
-
 module.exports = countAnimals;
