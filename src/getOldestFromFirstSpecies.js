77343f74f3d8173121077f491f678ed68ad0df0d
João Fabris <jvfabris21@gmail.com>
2022-05-30 21:54:24 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findEmployee = employees.find((employee) => employee.id === id);
+  const findFirstSpecie = findEmployee.responsibleFor[0];
+  const getAnimal = species.find((animal) => animal.id === findFirstSpecie).residents;
+  const findOldestAnimal = getAnimal.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+  return Object.values(findOldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
